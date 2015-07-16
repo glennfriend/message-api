7021219cacf6190a875e5fef7413b30a9baa2957
root <root@training3.training3>
2015-07-16 03:30:21 -0700

    update

--- app/config/template/hipchat.php
+++ app/config/template/hipchat.php
@@ -1,9 +1,15 @@
 <?php
 
+/**
+ *  hipchat v2 API
+ */
 return [
 
     /**
      *  API key
+     *
+     *  get key from https://hipchat.com/account/api
+     *
      */
     'key' => 'please-modify-the-value',
 
