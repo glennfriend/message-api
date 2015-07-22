73831a9319fa8a9d85a8675a2eefd1f5dfa88ba1
root <root@training3.training3>
2015-07-21 19:50:30 -0700

    update display page

--- app/display_mods/IndexController.php
+++ app/display_mods/IndexController.php
@@ -5,22 +5,25 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
-        $channel = InputBrg::get('channel');
-        $page    = (int) InputBrg::get('page');
-        if ( $channel ) {
+        $channel    = InputBrg::get('channel');
+        $page       = (int) InputBrg::get('page');
+        $myMessages = [];
+        $rowCount   = 0;
+        $messages   = new Messages();
+
+        if ($channel) {
             $options = array(
                 'channel'   => $channel,
                 '_page'     => $page
             );
-            $messages   = new Messages();
-            $myMessages = $messages->findMessages( $options );
-            $rowCount   = $messages->numFindMessages( $options );
-        } 
+            $myMessages = $messages->findMessages($options);
+            $rowCount   = $messages->numFindMessages($options);
+        }
 
         $pageLimit = new PageLimit();
-        $pageLimit->setBaseUrl( '' );  // 請使用完整的 mca 命名
-        $pageLimit->setRowCount( $rowCount );
-        $pageLimit->setPage( $page );
+        $pageLimit->setBaseUrl('');
+        $pageLimit->setRowCount($rowCount);
+        $pageLimit->setPage($page);
         $pageLimit->setParams(array(
             'channel' => $channel,
         ));
@@ -28,25 +31,7 @@ class IndexController extends ControllerBase
         $this->view->setVars(array(
             'pageLimit' => $pageLimit,
             'messages' => $myMessages,
-            'allChannel' => $this->getAllChannel(),
+            'allChannel' => $messages->getAllChannel(),
         ));
     }
-
-    public function getAllChannel()
-    {
-        $put = '';
-
-        $messages = new Messages();
-        $allChannel = $myMessages = $messages->getAllChannel();
-        foreach ( $allChannel as $channel ) {
-            $url = url('', array('channel'=>$channel));
-            $put .= '<li><a href="'. $url .'">'. $channel .'</a></li>';
-        }
-        
-        if ( !$put ) {
-            return '';
-        }
-        return '<ul>' . $put . '</ul>';
-    }
-
 }
