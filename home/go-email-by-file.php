a9fc4ae6c0f7e2e217b9a3a926064f75238809fe
root <root@training3.training3>
2015-10-02 04:34:30 -0700

    Add go-email log

--- home/go-email-by-file.php
+++ home/go-email-by-file.php
@@ -22,13 +22,18 @@ try {
 // --------------------------------------------------------------------------------
 //
 // --------------------------------------------------------------------------------
-$file = getFile();
+$txt  = getTxt();
+$file = getFile($txt);
 $info = parseFile($file);
 $result = sendEmail( $info->from, $info->to, $info->message, $info->footer );
-$result = true;
-if ( $result ) {
-    removeFile($file);
+if ($result) {
+    toLog("sned success - {$txt}");
+    removeTxt($txt);
 }
+else {
+    toLog("sned fail - {$txt}");
+}
+
 exit;
 
 
@@ -37,20 +42,39 @@ exit;
 // --------------------------------------------------------------------------------
 //
 // --------------------------------------------------------------------------------
-function getFile()
+function toLog($message)
+{
+    if (is_object($message) || is_array($message)) {
+        $message = print_r($message, true);
+    }
+
+    $logFile = Config::get('app.base.path') . '/var/log/go-email.log';
+    file_put_contents( $logFile, $message."\n", FILE_APPEND );
+}
+
+function getTxt()
 {
     if ( !isset($_GET['do']) ) {
         // 不正確的參數值
         exit;
     }
-    $do = $_GET['do'];
+    $txt = $_GET['do'];
 
-    $file = Config::get('app.base.path') . '/var/go-email/'.$do;
+    // security check
+    // TODO: please check a-zA-Z0-9 @ 一個 .
+    // 未完成
+
+    return $txt;
+}
+
+function getFile($txt)
+{
+    $basePath = Config::get('app.base.path');
+    $file = "{$basePath}/var/go-email/{$txt}";
     if ( !file_exists($file) ) {
-        // 目標檔案不存在
+        toLog('file not found - {$txt}');
         exit;
     }
-
     return $file;
 }
 
@@ -60,19 +84,22 @@ function parseFile($file)
     $info = json_decode($content);
     if ( !$info || !is_object($info) ) {
         // 格式不正確
+        toLog("parse json file fail - {$file}");
         exit;
     }
     if (    !isset($info->from    )
          || !isset($info->to      )
-         || !isset($info->message )
-         || !isset($info->footer  ) ) {
+         || !isset($info->message ) ) {
         // 格式不正確
+        toLog("lack of information in {$file}");
+        toLog("content:");
+        toLog($info);
         exit;
     }
     return $info;
 }
 
-function sendEmail($from, $to, $message, $footer)
+function sendEmail($from, $to, $message, $footer='')
 {
     $mail = new PHPMailer;
     $mail->CharSet  = "utf-8";  
@@ -89,8 +116,11 @@ function sendEmail($from, $to, $message, $footer)
     return true;
 }
 
-function removeFile($file)
+function removeTxt($txt)
 {
-    FileHelper::remove($file);
+    $basePath = Config::get('app.base.path');
+    $from = getFile($txt);
+    $to   = "{$basePath}/var/go-email-done/{$txt}";
+    rename($from, $to);
 }
 
